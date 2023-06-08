87742b6520d22ec4504540a2b6281250df3b30fc
Alfawigga <alfonsojiron@gmail.com>
2023-06-07 20:05:41 -0600

    V1
    Simple Text Changes

--- components/blocks.tsx
+++ components/blocks.tsx
@@ -7,7 +7,7 @@ export default function Blocks() {
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-20">
             <h2 className="h2 mb-4">We analyze our customer workflows to provide the best solutions.</h2>
-            <p className="text-xl text-gray-400">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <p className="text-xl text-gray-400">By leveraging industry trends, emerging technologies, and AI-driven insights, we optimize processes, accelerate delivery, and unlock your business's untapped potential.</p>
           </div>
 
           {/* Items */}
@@ -20,8 +20,8 @@ export default function Blocks() {
                 <path className="stroke-current text-purple-100" d="M30 39.313l-4.18 2.197L27 34.628l-5-4.874 6.91-1.004L32 22.49l3.09 6.26L42 29.754l-3 2.924" strokeLinecap="square" strokeWidth="2" fill="none" fillRule="evenodd" />
                 <path className="stroke-current text-purple-300" d="M43 42h-9M43 37h-9" strokeLinecap="square" strokeWidth="2" />
               </svg>
-              <h4 className="h4 mb-2">Instant Features</h4>
-              <p className="text-lg text-gray-400 text-center">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.</p>
+              <h4 className="h4 mb-2">Customer-centric approach</h4>
+              <p className="text-lg text-gray-400 text-center">We prioritize understanding the unique needs and goals of our customers, ensuring that our software development solutions align with their specific requirements.</p>
             </div>
 
             {/* 2nd item */}
@@ -31,8 +31,8 @@ export default function Blocks() {
                 <path className="stroke-current text-purple-100" strokeWidth="2" strokeLinecap="square" d="M21 23h22v18H21z" fill="none" fillRule="evenodd" />
                 <path className="stroke-current text-purple-300" d="M26 28h12M26 32h12M26 36h5" strokeWidth="2" strokeLinecap="square" />
               </svg>
-              <h4 className="h4 mb-2">Instant Features</h4>
-              <p className="text-lg text-gray-400 text-center">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.</p>
+              <h4 className="h4 mb-2">Agile methodology</h4>
+              <p className="text-lg text-gray-400 text-center">We embrace agile principles to foster flexibility, collaboration, and adaptability throughout the software development lifecycle. This allows us to quickly respond to changing requirements and deliver high-quality solutions.</p>
             </div>
 
             {/* 3rd item */}
@@ -45,8 +45,8 @@ export default function Blocks() {
                   <circle className="stroke-current text-purple-100" cx="11" cy="11" r="11" />
                 </g>
               </svg>
-              <h4 className="h4 mb-2">Instant Features</h4>
-              <p className="text-lg text-gray-400 text-center">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.</p>
+              <h4 className="h4 mb-2">User Experience Focus</h4>
+              <p className="text-lg text-gray-400 text-center">We create intuitive and engaging software interfaces for enhanced user satisfaction.</p>
             </div>
 
             {/* 4th item */}
@@ -58,8 +58,8 @@ export default function Blocks() {
                   <circle className="stroke-current text-purple-300" cx="13" cy="9" r="3" />
                 </g>
               </svg>
-              <h4 className="h4 mb-2">Instant Features</h4>
-              <p className="text-lg text-gray-400 text-center">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.</p>
+              <h4 className="h4 mb-2">Innovation-driven Mindset</h4>
+              <p className="text-lg text-gray-400 text-center"> We leverage emerging technologies for cutting-edge and innovative solutions.</p>
             </div>
 
             {/* 5th item */}
@@ -71,8 +71,8 @@ export default function Blocks() {
                   <path className="stroke-current text-purple-300" d="M22 30h4v12h-4z" />
                 </g>
               </svg>
-              <h4 className="h4 mb-2">Instant Features</h4>
-              <p className="text-lg text-gray-400 text-center">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.</p>
+              <h4 className="h4 mb-2">Quality Assurance</h4>
+              <p className="text-lg text-gray-400 text-center">We ensure reliability, performance, and security through rigorous testing and QA processes.</p>
             </div>
 
             {/* 6th item */}
@@ -85,8 +85,8 @@ export default function Blocks() {
                   <path className="stroke-current text-purple-100" d="M8.86 1.18C3.8 1.988 0 5.6 0 10c0 5 4.9 9 11 9a10.55 10.55 0 003.1-.4L20 21l-.6-5.2a9.125 9.125 0 001.991-2.948" />
                 </g>
               </svg>
-              <h4 className="h4 mb-2">Instant Features</h4>
-              <p className="text-lg text-gray-400 text-center">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat.</p>
+              <h4 className="h4 mb-2">Transparency</h4>
+              <p className="text-lg text-gray-400 text-center">We maintain open and proactive communication to foster strong partnerships with our clients.</p>
             </div>
 
           </div>
--- components/zigzag.tsx
+++ components/zigzag.tsx
@@ -12,9 +12,9 @@ export default function Zigzag() {
 
           {/* Section header */}
           <div className="max-w-3xl mx-auto text-center pb-12 md:pb-16">
-            <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-green-600 bg-green-200 rounded-full mb-4">Reach goals that matter</div>
-            <h1 className="h2 mb-4">One product, unlimited solutions</h1>
-            <p className="text-xl text-gray-400">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit laborum — semper quis lectus nulla.</p>
+            <div className="inline-flex text-sm font-semibold py-1 px-3 m-2 text-green-600 bg-green-200 rounded-full mb-4">Create Software That Matters</div>
+            <h1 className="h2 mb-4">One Team, unlimited solutions</h1>
+            <p className="text-xl text-gray-400">Our commitment to meeting project deadlines and staying on schedule ensures smooth execution and customer satisfaction.</p>
           </div>
 
           {/* Items */}
